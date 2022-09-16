3c317216f4d8f8f1afedacffdd5bbedacf7f7dd7
LU SIFE <michaelleix@outlook.com>
2022-09-16 12:00:43 -0600

    testing

--- obtain_issue_info.sh
+++ obtain_issue_info.sh
@@ -1,2 +1,4 @@
 local  new_entry=" echo ${{ github.event.issue.title }} | tr " " "-""
 echo "$new_entry"
+
+#sed -inr '/<!-- INDEX MARKER -->/ i - 🟢 [${{ github.event.issue.title }}](# "bash obtain_issue_info.sh") (Available)<br>'' README.md
