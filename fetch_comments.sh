c89e4d188484e483a5ea92e6c647fb0056cc99d4
LU SIFE <michaelleix@outlook.com>
2022-09-16 21:37:40 -0600

    Update fetch_comments.sh

--- fetch_comments.sh
+++ fetch_comments.sh
@@ -1,2 +1,5 @@
-content="wget ${{ github.event.issue.comments_url }} -q -O -)"
+comment_test_url="$(echo "${{ github.event.issue.comments_url }}")"
+echo "$comment_test_url"
+
+content="wget "${comment_test_url}" -q -O -"
 echo $content
