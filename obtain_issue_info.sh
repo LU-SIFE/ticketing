fe82f06c2766e29b2779ae368112b6ce6d80217e
LU SIFE <michaelleix@outlook.com>
2022-09-16 18:16:54 -0600

    removed bash file

--- obtain_issue_info.sh
+++
@@ -1,3 +0,0 @@
-echo "${{ github.event.issue.title }}" | tr " " "-"
-
-#sed -inr '/<!-- INDEX MARKER -->/ i - 🟢 [${{ github.event.issue.title }}](# "bash obtain_issue_info.sh") (Available)<br>'' README.md
