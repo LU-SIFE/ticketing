67f87194048749c8991e55a92f5a7bfad5987f33
LU SIFE <michaelleix@outlook.com>
2022-09-16 21:43:43 -0600

    Update fetch_comments.sh

--- fetch_comments.sh
+++ fetch_comments.sh
@@ -3,5 +3,4 @@ new_var=$1
 comment_test_url="$(echo "$new_var")"
 echo "$comment_test_url"
 
-content="wget "${comment_test_url}" -q -O -"
-echo $content
+echo ""wget "${comment_test_url}" -q -O -""
